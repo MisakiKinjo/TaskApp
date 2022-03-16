86a6fc759f10c1375d302842a29074c8200d3cdc
Misaki Kinjo <misaki0827bitodemi@gmail.com>
2022-03-16 18:19:05 +0900

    Initial Commit

---
+++ taskApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  taskApp
+//
+//  Created by 金城美咲 on 2022/03/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
